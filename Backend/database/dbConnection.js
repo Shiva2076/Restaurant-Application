97726151cccd2dc04b38e329b0df834122a7a4ee
Shiva <115081717+Shiva2076@users.noreply.github.com>
2024-09-11 15:25:45 +0530

    Initial commit

---
+++ Backend/database/dbConnection.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+export const dbConnection = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+    throw new Error("Failed to connect to the database");
+  }
+};
